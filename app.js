921681d3f4380692ea776731c9c8f812924700af
knightbrilliance <alonsogutierrez94@gmail.com>
2017-09-30 12:03:10 -0400

    modify visualization size to a better fit

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //height of each row in the heatmap
-var height = 10;
+var height = 7;
 
 //width of each column in the heatmap
-var width = 10;
+var width = 7;
 
 //margins
 var marginLeft = 0
@@ -14,26 +14,18 @@ var colors = ['#ffcc66','#ff6666','#000000']
 //attach a SVG element to the document's body
 var mySVG = d3.select('#chart')
    .append('svg')
-   .attr('class','col-md-9')
-   .attr('display','block')
-   .attr('width', (width * cols.length)+ 500) 
-   .attr('height', (height * rows.length + 500))
-   .style('margin','auto')
+   .attr('class','col-md-6')
+   .attr('width', (width * cols.length)) 
+   .attr('height', (height * rows.length + 300))
    .style('background', 'white')
 
 //attach diplay div element to the document's body
 var myDiv = d3.select('#chart')
-   .append('div')
-   .attr('class', 'col-md-3')
+   .append('h2')
+   .attr('class', 'col-md-6')
    .attr('id', 'text-box')
-   .attr('font','helvetica')
-   .attr('font-size', 30)
-   
-// var title = mySVG.append('svg:text')
-//    .attr('y', 50)
-//    .attr('x',125)
-//    .text('Genetic Expression Level in Separate Leukemia Subtype.')
-//    .attr('font-size', 20)
+   .style('display','block')
+   .style('margin-top', '3em')
 
 //define a color scale using the min and max expression values
 var colorScale = d3.scale.linear()
@@ -55,10 +47,10 @@ var heatmapRects = heatmapRow
    .attr('width',width)
    .attr('height',height)
    .attr('x', function(d) {
-      return (d[2] * width) + 150;
+      return (d[2] * width) + 100;
    })
    .attr('y', function(d) {
-      return (d[1] * height) + 250;
+      return (d[1] * height) + 200;
    })
    .style('fill',function(d) {
       return colorScale(d[0]);
@@ -69,11 +61,11 @@ var columnLabel = mySVG.selectAll('.colLabel')
    .data(cols)
    .enter()
    .append('svg:text')
-   .attr('x', -235)
+   .attr('x', -200 + height)
    .attr('y', function(d,i) {
-      return ((i*10)+160);
+      return ((i*height)+100+5);
    })
-   .attr('font-size',10)
+   .attr('font-size',width)
    .attr('class','label')
    .attr('transform', 'rotate(-90)')
    .style('top',100)
@@ -86,12 +78,12 @@ var rowLabel = mySVG.selectAll('.rowLabel')
    .data(rows)
    .enter()
    .append('svg:text')
-   .attr('x', 125)
+   .attr('x', 100 - width -5)
    .attr('y', function(d,i) {
-      return ((i*height)+250+height);
+      return ((i*height)+200+height);
    })
    .attr('class','label')
-   .attr('font-size', 10)
+   .attr('font-size', height)
    .style('top',100)
    .text(function(d) {
       return d;
@@ -111,7 +103,7 @@ var expLab = d3.select('#v')
 //position scale for legends
 var positionScale = d3.scale.linear()
    .domain([minData,maxData])
-   .range([100,width*cols.length + 55])
+   .range([100,385])
 
 //expression level legend 
 var legendA = mySVG.selectAll('.legendA')
@@ -120,12 +112,8 @@ var legendA = mySVG.selectAll('.legendA')
    .append('g')
    .attr('class', 'legend')
    .append('rect')
-   .attr('x', function(d){
-      return positionScale(d);
-   })
-   .attr('y', 950)
    .attr('width', function(d){
-      return (width*cols.length/legendCols.length);
+      return (30);
    })
    .attr('height', 25)
    .style('fill', function(d){
@@ -133,38 +121,38 @@ var legendA = mySVG.selectAll('.legendA')
    })
    .attr('class','label')
    .attr('x', function(d){
-      return (positionScale(d)+50);
+      return (positionScale(d));
    })
-   .attr('y', (height*72 + 325));
+   .attr('y', (height*72 + 230));
 
 var patientText = mySVG.append('svg:text')
    .attr('transform', 'rotate(-90)')
    .attr('x',-(height * rows.length + 500)/2)
    .attr('y',75)
-   .attr('font-size',25)
+   .attr('font-size',width*2)
    .text('Patients\' numbers')
 
 var lowGLSSLegendText = mySVG.append('svg:text')
-   .attr('x',positionScale(minData)+50)
+   .attr('x',positionScale(minData))
    .attr('y',100)
-   .text('Genes of Lowest Statistical Significance')
+   .text('Lowest Statistical Significance')
    .attr('font-size',10)
 
 var highGLSSLegendText = mySVG.append('svg:text')
-   .attr('x',positionScale(maxData) - 85)
+   .attr('x',positionScale(maxData) - 105)
    .attr('y',100)
-   .text('Genes of Highest Statistical Significance')
+   .text('Highest Statistical Significance')
    .attr('font-size',10)
 
 var lowEVLegendText = mySVG.append('svg:text')
-   .attr('x',positionScale(minData) + 50)
-   .attr('y',(height*72 + 300))
+   .attr('x',positionScale(minData))
+   .attr('y',(height*72 + 225))
    .text('Lowest Expression Level')
    .attr('font-size',10)
 
 var highEVLegendText = mySVG.append('svg:text')
-   .attr('x',positionScale(maxData)-15)
-   .attr('y',(height*72 + 300))
+   .attr('x',positionScale(maxData)-85)
+   .attr('y',(height*72 + 225))
    .text('Highest Expression Level')
    .attr('font-size',10)
 
@@ -176,7 +164,7 @@ var legendB = mySVG.selectAll('.legendB')
    .append('g')
    .attr('class','legend')
    .append('rect')
-   .attr('x',cols.length*width + 175) 
+   .attr('x',cols.length*width + 125) 
    .attr('y',function(d){
       return ((d[0] * 25) + 250)
    })
@@ -187,23 +175,23 @@ var legendB = mySVG.selectAll('.legendB')
    });
 
 var legendAMLLabel = mySVG.append('svg:text')
-   .attr('x', positionScale(maxData) + 150 - (width/2))
-   .attr('y', 250+(height/2))
+   .attr('x', positionScale(maxData) + 75 - (width/2))
+   .attr('y', 255+(height/2))
    .text('ALL')
    .attr('font-size',10)
 
 var legendALLLabel = mySVG.append('svg:text')
-   .attr('x', positionScale(maxData) + 150 - (width/2))
-   .attr('y', 275+(height/2))
+   .attr('x', positionScale(maxData) + 75 - (width/2))
+   .attr('y', 280+(height/2))
    .text('AML')
    .attr('font-size',10)
 
 var legendBTitle = mySVG.append('svg:text')
-   .attr('x', positionScale(maxData) + 110)
+   .attr('x', positionScale(maxData) + 50)
    .attr('y', 225)
    .style('font-size',15)
    .style('font', 'helvetica')
-   .text('Leukemia Subtype')
+   .text('Subtype')
 
 //heatmap mouse events
 heatmapRow
@@ -230,23 +218,64 @@ heatmapRow
          .style('display','none')
    });
 
-//column label mouse events
 columnLabel
-   .on('click', function (d,i){
+   .on("click", function(d,i){
+         
+         var active = this.active ? false : true;
+         if (active == true) {
+         d3.select("#text-box")
+            .text('Gene Description:')
+            .append('hr')
+            .append('h4')
+            .text(gnsAndDescripts[d])
+         };
+
+         if (active == false) {
+            d3.select('#text-box')
+               .text('')
+         };
+         this.active = active;
+   }).on('mouseover', function(d,i) {
+
       d3.select(this)
          .attr('stroke-width',1)
-         .attr('stroke', 'black')
+         .attr('stroke','Black')
 
-      d3.select('#text-box')
-         .append('h2')
-         .style('margin-top', '2.5em')
-         .text(gnsAndDescripts[d]);
-         
-}).on('mouseout', function (d,i){
+      output = '<b>' + rows[i] + '</b><br>';
+      for (var j = 0 , count = data[i].length; j < count; j ++ ) {
+         output += data[i][j][0] + ', ';
+      }
+      expLab
+         .style('margin','auto')
+         .style('display','block')
+         .html(output.substring(0,output.length));
+   }).on('mouseout', function(d,i) {
+
       d3.select(this)
          .attr('stroke-width',0)
-         .attr('stroke', 'none');
+         .attr('stroke','none')
+      expLab
+         .style('display','none')
+   });
 
-      d3.select('#text-box')
-         .html('');
-})
+   
+columnLabel
+   .on("click", function(d,i){
+         
+         var active = this.active ? false : true;
+         if (active == true) {
+         d3.select("#text-box")
+            .text('Gene Description:')
+            .append('hr')
+            .append('h4')
+            .text(gnsAndDescripts[d])
+            .style('text-align','left')
+         };
+
+         if (active == false) {
+            d3.select('#text-box')
+               .text('')
+         };
+         this.active = active;
+});
+
